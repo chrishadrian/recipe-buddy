b775f18599dd6326d4232398ec6a0fc7d43b23e8
Christopher Wong <chrishadrianwong@gmail.com>
2023-03-06 00:39:39 -0800

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+import { ReceiptOutlined, MenuOutlined, CalendarMonthOutlined, DashboardOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 function App() {
+  const { collapseSidebar, toggleSidebar, collapsed } = useProSidebar();
+
+  const menuItemStyle = "text-white "
+
   return (
-    <div></div>
+    <div id="app" style={{ height: "100vh", display: "flex" }}>
+      <Sidebar style={{ height: "100vh" }} backgroundColor="#8BC34A">
+        <Menu>
+          <MenuItem
+            icon={<MenuOutlined />}
+            onClick={() => {
+              collapseSidebar();
+            }}
+          >
+            <h2>Fit Tracker</h2>
+          </MenuItem>
+          <MenuItem icon={<DashboardOutlined />}>Dashboard</MenuItem>
+          <MenuItem icon={<ReceiptOutlined />}>Recipes</MenuItem>
+          <MenuItem icon={<CalendarMonthOutlined />}>Planner</MenuItem>
+          <MenuItem icon={<ShoppingCartOutlined />}>Shopping List</MenuItem>
+        </Menu>
+      </Sidebar>
+      <main>
+        <h1 style={{ color: "white", marginLeft: "5rem" }}>
+          React-Pro-Sidebar
+        </h1>
+      </main>
+    </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      primary: {
+        DEFAULT: '#8BC34A',
+      },
+      secondary: {
+        DEFAULT: '#689F38',
+      },
+      tertiary: {
+        DEFAULT: '#558B2F',
+      },
+      white: {
+        DEFAULT: '#FFFFFF',
+      },
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
