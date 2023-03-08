e8a346a550f5805bedffe2c606829051e2bfd03b
Christopher Wong <chrishadrianwong@gmail.com>
2023-03-08 10:06:13 -0800

    Added routes and sidebar to App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
+import React from 'react';
 import './App.css';
-import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
-import { ReceiptOutlined, MenuOutlined, CalendarMonthOutlined, DashboardOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Dashboard } from './pages/dashboard';
+import { StyledSidebar } from './component/sidebar';
+
 function App() {
-  const { collapseSidebar, toggleSidebar, collapsed } = useProSidebar();
-
-  const menuItemStyle = "text-white "
-
   return (
-    <div id="app" style={{ height: "100vh", display: "flex" }}>
-      <Sidebar style={{ height: "100vh" }} backgroundColor="#8BC34A">
-        <Menu>
-          <MenuItem
-            icon={<MenuOutlined />}
-            onClick={() => {
-              collapseSidebar();
-            }}
-          >
-            <h2>Fit Tracker</h2>
-          </MenuItem>
-          <MenuItem icon={<DashboardOutlined />}>Dashboard</MenuItem>
-          <MenuItem icon={<ReceiptOutlined />}>Recipes</MenuItem>
-          <MenuItem icon={<CalendarMonthOutlined />}>Planner</MenuItem>
-          <MenuItem icon={<ShoppingCartOutlined />}>Shopping List</MenuItem>
-        </Menu>
-      </Sidebar>
-      <main>
-        <h1 style={{ color: "white", marginLeft: "5rem" }}>
-          React-Pro-Sidebar
-        </h1>
-      </main>
-    </div>
+    <Router>
+      <div style={{ height: "100vh", display: "flex" }}>
+        <StyledSidebar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/about" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
