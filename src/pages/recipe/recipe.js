484ad7c15202f47a049743b652699754de4c99a1
Christopher Wong <chrishadrianwong@gmail.com>
2023-03-10 16:36:05 -0800

    Separated searchbar

--- src/pages/recipe/recipe.js
+++ src/pages/recipe/recipe.js
@@ -1,20 +1,13 @@
 import React, { useState } from 'react';
-import { Box, Button, InputAdornment, TextField, Typography, useTheme } from '@mui/material'
+import { Box, Button, Typography, useTheme } from '@mui/material'
 import { recipes } from './constant';
-import { Add, Search } from '@mui/icons-material';
-import { useProSidebar } from 'react-pro-sidebar';
+import { Add } from '@mui/icons-material';
 import RecipeContent from './components/RecipeContent';
+import { SearchBar } from './components/SearchBar';
 
 const RecipeList = () => {
   const { palette } = useTheme();
   const [filteredRecipes, setFilteredRecipes] = useState(recipes)
-
-  const handleSearchChange = (e) => {
-    const searchText = e.target.value
-    setFilteredRecipes(recipes.filter((recipe) =>
-      recipe.title.toLowerCase().includes(searchText.toLowerCase())
-    ))
-  }
 
   return (
     <div className='flex flex-col m-5 mx-14 w-full'>
@@ -22,28 +15,7 @@ const RecipeList = () => {
         Your recipes
       </Typography>
       <Box className="flex justify-between">
-        <TextField
-          label="Search"
-          size="small"
-          onChange={handleSearchChange}
-          sx={{
-            borderRadius: 20,
-            width: '15rem',
-            color: 'gray',
-            fontWeight: '500',
-            '& .MuiInputBase-root': {
-              borderRadius: 20,
-              pl: 1
-            },
-          }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Search />
-              </InputAdornment>
-            ),
-          }}
-        />
+        <SearchBar recipes={recipes} setFilteredRecipes={setFilteredRecipes}/>
         <Button variant='contained' startIcon={<Add />} sx={{ borderRadius: '20px', width: "10rem", mr:2}}>Add Item</Button>
       </Box>
       <RecipeContent filteredRecipes={filteredRecipes} palette={palette} />
