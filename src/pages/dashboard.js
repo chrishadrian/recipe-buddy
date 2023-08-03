d16709d6bf7e82e9019c27bbf4f870ba110437f2
Christopher Wong <chrishadrianwong@gmail.com>
2023-08-02 21:13:50 -0700

    Added buttons to landing page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,5 @@
 import React from "react";
-import LoginButton from "../component/Login";
 
 export const Dashboard = () => {
-	return (
-		<div>
-			<LoginButton />
-		</div>
-	);
+	return <div>Dashboard</div>;
 };
