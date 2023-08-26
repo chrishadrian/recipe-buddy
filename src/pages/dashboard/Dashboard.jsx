0a3589d6c50dd7981d3ad98a22c7f89bbb87467b
Christopher Wong <chrishadrianwong@gmail.com>
2023-08-26 09:45:28 +0700

    Updated dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,24 @@
 import { Box, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import RecipeTable from './components/RecipeTable';
 import ShoppingList from './components/ShoppingList';
 import SignIn from '../../component/SignIn';
 
 export default function Dashboard() {
+	const { user } = useAuth0();
+	const [username, setUsername] = useState('');
+
+	useEffect(() => {
+		console.log('User: ', user);
+		if (user) {
+			setUsername(user.name.charAt(0).toUpperCase() + user.name.slice(1));
+		}
+	}, [user]);
 	return (
 		<Box className='p-8 w-[65%]'>
 			<Typography variant='h2' color='primary' sx={{ mb: 3 }}>
-				Welcome, Christopher
+				{`Welcome, ${username}`}
 			</Typography>
 			<Box className='flex w-full justify-between'>
 				<Box className=''>
