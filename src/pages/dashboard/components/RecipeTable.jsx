ca1e0a4e0186eb58f333fc4705648de7c3f62f5a
Christopher Wong <chrishadrianwong@gmail.com>
2023-08-28 23:15:07 +0700

    Updated recipetable

--- src/pages/dashboard/components/RecipeTable.jsx
+++ src/pages/dashboard/components/RecipeTable.jsx
@@ -1,24 +1,27 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import { alpha } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import DeleteIcon from "@mui/icons-material/Delete";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import { visuallyHidden } from "@mui/utils";
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable object-curly-newline */
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
 
 function createData(name, calories, fat, carbs, protein) {
 	return {
@@ -31,19 +34,19 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-	createData("Cupcake", 305, 3.7, 67, 4.3),
-	createData("Donut", 452, 25.0, 51, 4.9),
-	createData("Eclair", 262, 16.0, 24, 6.0),
-	createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-	createData("Gingerbread", 356, 16.0, 49, 3.9),
-	createData("Honeycomb", 408, 3.2, 87, 6.5),
-	createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-	createData("Jelly Bean", 375, 0.0, 94, 0.0),
-	createData("KitKat", 518, 26.0, 65, 7.0),
-	createData("Lollipop", 392, 0.2, 98, 0.0),
-	createData("Marshmallow", 318, 0, 81, 2.0),
-	createData("Nougat", 360, 19.0, 9, 37.0),
-	createData("Oreo", 437, 18.0, 63, 4.0),
+	createData('Cupcake', 305, 3.7, 67, 4.3),
+	createData('Donut', 452, 25.0, 51, 4.9),
+	createData('Eclair', 262, 16.0, 24, 6.0),
+	createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+	createData('Gingerbread', 356, 16.0, 49, 3.9),
+	createData('Honeycomb', 408, 3.2, 87, 6.5),
+	createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+	createData('Jelly Bean', 375, 0.0, 94, 0.0),
+	createData('KitKat', 518, 26.0, 65, 7.0),
+	createData('Lollipop', 392, 0.2, 98, 0.0),
+	createData('Marshmallow', 318, 0, 81, 2.0),
+	createData('Nougat', 360, 19.0, 9, 37.0),
+	createData('Oreo', 437, 18.0, 63, 4.0),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -57,7 +60,7 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-	return order === "desc"
+	return order === 'desc'
 		? (a, b) => descendingComparator(a, b, orderBy)
 		: (a, b) => -descendingComparator(a, b, orderBy);
 }
@@ -80,46 +83,39 @@ function stableSort(array, comparator) {
 
 const headCells = [
 	{
-		id: "name",
+		id: 'name',
 		numeric: false,
 		disablePadding: true,
-		label: "Menu",
+		label: 'Menu',
 	},
 	{
-		id: "calories",
+		id: 'calories',
 		numeric: true,
 		disablePadding: false,
-		label: "Calories",
+		label: 'Calories',
 	},
 	{
-		id: "fat",
+		id: 'fat',
 		numeric: true,
 		disablePadding: false,
-		label: "Fat (g)",
+		label: 'Fat (g)',
 	},
 	{
-		id: "carbs",
+		id: 'carbs',
 		numeric: true,
 		disablePadding: false,
-		label: "Carbs (g)",
+		label: 'Carbs (g)',
 	},
 	{
-		id: "protein",
+		id: 'protein',
 		numeric: true,
 		disablePadding: false,
-		label: "Protein (g)",
+		label: 'Protein (g)',
 	},
 ];
 
 function EnhancedTableHead(props) {
-	const {
-		onSelectAllClick,
-		order,
-		orderBy,
-		numSelected,
-		rowCount,
-		onRequestSort,
-	} = props;
+	const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
 	const createSortHandler = (property) => (event) => {
 		onRequestSort(event, property);
 	};
@@ -127,37 +123,31 @@ function EnhancedTableHead(props) {
 	return (
 		<TableHead>
 			<TableRow>
-				<TableCell padding="checkbox">
+				<TableCell padding='checkbox'>
 					<Checkbox
-						color="primary"
-						indeterminate={
-							numSelected > 0 && numSelected < rowCount
-						}
+						color='primary'
+						indeterminate={numSelected > 0 && numSelected < rowCount}
 						checked={rowCount > 0 && numSelected === rowCount}
 						onChange={onSelectAllClick}
-						inputProps={{
-							"aria-label": "select all desserts",
-						}}
+						inputProps={{ 'aria-label': 'select all desserts' }}
 					/>
 				</TableCell>
 				{headCells.map((headCell) => (
 					<TableCell
 						key={headCell.id}
-						align={headCell.numeric ? "right" : "left"}
-						padding={headCell.disablePadding ? "none" : "normal"}
+						align={headCell.numeric ? 'right' : 'left'}
+						padding={headCell.disablePadding ? 'none' : 'normal'}
 						sortDirection={orderBy === headCell.id ? order : false}
 					>
 						<TableSortLabel
 							active={orderBy === headCell.id}
-							direction={orderBy === headCell.id ? order : "asc"}
+							direction={orderBy === headCell.id ? order : 'asc'}
 							onClick={createSortHandler(headCell.id)}
 						>
 							{headCell.label}
 							{orderBy === headCell.id ? (
-								<Box component="span" sx={visuallyHidden}>
-									{order === "desc"
-										? "sorted descending"
-										: "sorted ascending"}
+								<Box component='span' sx={visuallyHidden}>
+									{order === 'desc' ? 'sorted descending' : 'sorted ascending'}
 								</Box>
 							) : null}
 						</TableSortLabel>
@@ -172,7 +162,7 @@ EnhancedTableHead.propTypes = {
 	numSelected: PropTypes.number.isRequired,
 	onRequestSort: PropTypes.func.isRequired,
 	onSelectAllClick: PropTypes.func.isRequired,
-	order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+	order: PropTypes.oneOf(['asc', 'desc']).isRequired,
 	orderBy: PropTypes.string.isRequired,
 	rowCount: PropTypes.number.isRequired,
 };
@@ -186,42 +176,28 @@ function EnhancedTableToolbar(props) {
 				pl: { sm: 2 },
 				pr: { xs: 1, sm: 1 },
 				...(numSelected > 0 && {
-					bgcolor: (theme) =>
-						alpha(
-							theme.palette.primary.main,
-							theme.palette.action.activatedOpacity
-						),
+					bgcolor: (theme) => alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
 				}),
 			}}
 		>
 			{numSelected > 0 ? (
-				<Typography
-					sx={{ flex: "1 1 100%" }}
-					color="inherit"
-					variant="subtitle1"
-					component="div"
-				>
+				<Typography sx={{ flex: '1 1 100%' }} color='inherit' variant='subtitle1' component='div'>
 					{numSelected} selected
 				</Typography>
 			) : (
-				<Typography
-					sx={{ flex: "1 1 100%" }}
-					variant="h6"
-					id="tableTitle"
-					component="div"
-				>
+				<Typography sx={{ flex: '1 1 100%' }} variant='h6' id='tableTitle' component='div'>
 					Recipe
 				</Typography>
 			)}
 
 			{numSelected > 0 ? (
-				<Tooltip title="Delete">
+				<Tooltip title='Delete'>
 					<IconButton>
 						<DeleteIcon />
 					</IconButton>
 				</Tooltip>
 			) : (
-				<Tooltip title="Filter list">
+				<Tooltip title='Filter list'>
 					<IconButton>
 						<FilterListIcon />
 					</IconButton>
@@ -231,20 +207,18 @@ function EnhancedTableToolbar(props) {
 	);
 }
 
-EnhancedTableToolbar.propTypes = {
-	numSelected: PropTypes.number.isRequired,
-};
+EnhancedTableToolbar.propTypes = { numSelected: PropTypes.number.isRequired };
 
 export default function RecipeTable() {
-	const [order, setOrder] = React.useState("asc");
-	const [orderBy, setOrderBy] = React.useState("calories");
+	const [order, setOrder] = React.useState('asc');
+	const [orderBy, setOrderBy] = React.useState('calories');
 	const [selected, setSelected] = React.useState([]);
 	const [page, setPage] = React.useState(0);
 	const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
 	const handleRequestSort = (event, property) => {
-		const isAsc = orderBy === property && order === "asc";
-		setOrder(isAsc ? "desc" : "asc");
+		const isAsc = orderBy === property && order === 'asc';
+		setOrder(isAsc ? 'desc' : 'asc');
 		setOrderBy(property);
 	};
 
@@ -268,10 +242,7 @@ export default function RecipeTable() {
 		} else if (selectedIndex === selected.length - 1) {
 			newSelected = newSelected.concat(selected.slice(0, -1));
 		} else if (selectedIndex > 0) {
-			newSelected = newSelected.concat(
-				selected.slice(0, selectedIndex),
-				selected.slice(selectedIndex + 1)
-			);
+			newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
 		}
 
 		setSelected(newSelected);
@@ -289,28 +260,20 @@ export default function RecipeTable() {
 	const isSelected = (name) => selected.indexOf(name) !== -1;
 
 	// Avoid a layout jump when reaching the last page with empty rows.
-	const emptyRows =
-		page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+	const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
 	const visibleRows = React.useMemo(
-		() =>
-			stableSort(rows, getComparator(order, orderBy)).slice(
-				page * rowsPerPage,
-				page * rowsPerPage + rowsPerPage
-			),
-		[order, orderBy, page, rowsPerPage]
+		() => stableSort(rows, getComparator(order, orderBy))
+			.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+		[order, orderBy, page, rowsPerPage],
 	);
 
 	return (
-		<Box sx={{ width: "100%" }}>
-			<Paper sx={{ width: "100%", mb: 2 }}>
+		<Box sx={{ width: '100%' }}>
+			<Paper sx={{ width: '100%', mb: 2 }}>
 				<EnhancedTableToolbar numSelected={selected.length} />
 				<TableContainer>
-					<Table
-						sx={{ minWidth: 700 }}
-						aria-labelledby="tableTitle"
-						size={"medium"}
-					>
+					<Table sx={{ minWidth: 700 }} aria-labelledby='tableTitle' size='medium'>
 						<EnhancedTableHead
 							numSelected={selected.length}
 							order={order}
@@ -327,54 +290,33 @@ export default function RecipeTable() {
 								return (
 									<TableRow
 										hover
-										onClick={(event) =>
-											handleClick(event, row.name)
-										}
-										role="checkbox"
+										onClick={(event) => handleClick(event, row.name)}
+										role='checkbox'
 										aria-checked={isItemSelected}
 										tabIndex={-1}
 										key={row.name}
 										selected={isItemSelected}
-										sx={{ cursor: "pointer" }}
+										sx={{ cursor: 'pointer' }}
 									>
-										<TableCell padding="checkbox">
+										<TableCell padding='checkbox'>
 											<Checkbox
-												color="primary"
+												color='primary'
 												checked={isItemSelected}
-												inputProps={{
-													"aria-labelledby": labelId,
-												}}
+												inputProps={{ 'aria-labelledby': labelId }}
 											/>
 										</TableCell>
-										<TableCell
-											component="th"
-											id={labelId}
-											scope="row"
-											padding="none"
-										>
+										<TableCell component='th' id={labelId} scope='row' padding='none'>
 											{row.name}
 										</TableCell>
-										<TableCell align="right">
-											{row.calories}
-										</TableCell>
-										<TableCell align="right">
-											{row.fat}
-										</TableCell>
-										<TableCell align="right">
-											{row.carbs}
-										</TableCell>
-										<TableCell align="right">
-											{row.protein}
-										</TableCell>
+										<TableCell align='right'>{row.calories}</TableCell>
+										<TableCell align='right'>{row.fat}</TableCell>
+										<TableCell align='right'>{row.carbs}</TableCell>
+										<TableCell align='right'>{row.protein}</TableCell>
 									</TableRow>
 								);
 							})}
 							{emptyRows > 0 && (
-								<TableRow
-									style={{
-										height: 53 * emptyRows,
-									}}
-								>
+								<TableRow style={{ height: 53 * emptyRows }}>
 									<TableCell colSpan={6} />
 								</TableRow>
 							)}
@@ -383,7 +325,7 @@ export default function RecipeTable() {
 				</TableContainer>
 				<TablePagination
 					rowsPerPageOptions={[5, 10, 25]}
-					component="div"
+					component='div'
 					count={rows.length}
 					rowsPerPage={rowsPerPage}
 					page={page}
