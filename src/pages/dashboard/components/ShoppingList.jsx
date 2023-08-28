c02a5453f535edab3b9f1e15498ad69de040a86e
Christopher Wong <chrishadrianwong@gmail.com>
2023-08-28 23:15:24 +0700

    Refactored ShoppingList

--- src/pages/dashboard/components/ShoppingList.jsx
+++ src/pages/dashboard/components/ShoppingList.jsx
@@ -1,20 +1,20 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { TableVirtuoso } from "react-virtuoso";
-import { Toolbar, Typography } from "@mui/material";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { TableVirtuoso } from 'react-virtuoso';
+import { Toolbar, Typography } from '@mui/material';
 
 const sample = [
-	["Frozen yoghurt", "1 pcs"],
-	["Ice cream sandwich", "3 pcs"],
-	["Eclair", "1 pcs"],
-	["Cupcake", "1 dozen"],
-	["Gingerbread", "1 dozen"],
+	['Frozen yoghurt', '1 pcs'],
+	['Ice cream sandwich', '3 pcs'],
+	['Eclair', '1 pcs'],
+	['Cupcake', '1 dozen'],
+	['Gingerbread', '1 dozen'],
 ];
 
 function createData(id, name, quantity) {
@@ -24,13 +24,13 @@ function createData(id, name, quantity) {
 const columns = [
 	{
 		width: 50,
-		label: "Name",
-		dataKey: "name",
+		label: 'Name',
+		dataKey: 'name',
 	},
 	{
 		width: 50,
-		label: "Quantity",
-		dataKey: "quantity",
+		label: 'Quantity',
+		dataKey: 'quantity',
 		numeric: true,
 	},
 ];
@@ -41,20 +41,11 @@ const rows = Array.from({ length: 10 }, (_, index) => {
 });
 
 const VirtuosoTableComponents = {
-	Scroller: React.forwardRef((props, ref) => (
-		<TableContainer component={Paper} {...props} ref={ref} />
-	)),
-	Table: (props) => (
-		<Table
-			{...props}
-			sx={{ borderCollapse: "separate", tableLayout: "fixed" }}
-		/>
-	),
+	Scroller: React.forwardRef((props, ref) => <TableContainer component={Paper} {...props} ref={ref} />),
+	Table: (props) => <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />,
 	TableHead,
 	TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
-	TableBody: React.forwardRef((props, ref) => (
-		<TableBody {...props} ref={ref} />
-	)),
+	TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
 };
 
 function fixedHeaderContent() {
@@ -63,12 +54,10 @@ function fixedHeaderContent() {
 			{columns.map((column) => (
 				<TableCell
 					key={column.dataKey}
-					variant="head"
-					align={column.numeric || false ? "right" : "left"}
+					variant='head'
+					align={column.numeric || false ? 'right' : 'left'}
 					style={{ width: column.width }}
-					sx={{
-						backgroundColor: "background.paper",
-					}}
+					sx={{ backgroundColor: 'background.paper' }}
 				>
 					{column.label}
 				</TableCell>
@@ -79,34 +68,26 @@ function fixedHeaderContent() {
 
 function rowContent(_index, row) {
 	return (
-		<React.Fragment>
+		<>
 			{columns.map((column) => (
-				<TableCell
-					key={column.dataKey}
-					align={column.numeric || false ? "right" : "left"}
-				>
+				<TableCell key={column.dataKey} align={column.numeric || false ? 'right' : 'left'}>
 					{row[column.dataKey]}
 				</TableCell>
 			))}
-		</React.Fragment>
+		</>
 	);
 }
 
-function EnhancedTableToolbar(props) {
+function EnhancedTableToolbar() {
 	return (
 		<Toolbar
 			sx={{
 				pl: { sm: 2 },
 				pr: { xs: 1, sm: 1 },
-				height: '14.5%'
+				height: '14.5%',
 			}}
 		>
-			<Typography
-				sx={{ flex: "1 1 100%" }}
-				variant="h6"
-				id="tableTitle"
-				component="div"
-			>
+			<Typography sx={{ flex: '1 1 100%' }} variant='h6' id='tableTitle' component='div'>
 				Shopping List
 			</Typography>
 		</Toolbar>
@@ -115,7 +96,7 @@ function EnhancedTableToolbar(props) {
 
 export default function EnhancedList() {
 	return (
-		<Paper style={{ height: "85.5%", width: "100%" }}>
+		<Paper style={{ height: '85.5%', width: '100%' }}>
 			<EnhancedTableToolbar />
 			<TableVirtuoso
 				data={rows}
