3d00935d278f069bc6f5c0a252382388e3251e45
Christopher Wong <chrishadrianwong@gmail.com>
2023-08-29 13:43:02 +0800

    Updated origin look

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import Dashboard from './pages/dashboard/Dashboard';
 import StyledSidebar from './component/Sidebar';
 import Recipe from './pages/recipe/Recipe';
 import Landing from './pages/Landing';
 
 function App() {
-	const isLogin = true;
+	const { isAuthenticated } = useAuth0();
 	return (
 		<Router>
 			<div style={{ height: '100vh', display: 'flex' }}>
-				{isLogin ? (
+				{isAuthenticated ? (
 					<>
 						<StyledSidebar />
 						<Routes>
