30e98a538137fb8fc88dd73885a7a78422c3d455
Christopher Wong <chrishadrianwong@gmail.com>
2023-09-01 06:58:54 -0700

    Added hasher for recipe

--- src/utils/hasher.js
+++ src/utils/hasher.js
@@ -1,7 +1,13 @@
 import { SHA256 } from 'crypto-js';
 
-export default function generateUserID(userAuth) {
+export function generateUserID(userAuth) {
 	const secret = userAuth.name + userAuth.email + userAuth.locale;
+	const hashedValue = SHA256(secret).toString();
+	return hashedValue;
+}
+
+export function generateRecipeID(recipe) {
+	const secret = recipe.title + recipe.description;
 	const hashedValue = SHA256(secret).toString();
 	return hashedValue;
 }
