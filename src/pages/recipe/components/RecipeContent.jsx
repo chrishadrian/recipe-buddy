f7c7b6be87a7bfbe4a003870011964b27dc82751
Christopher Wong <chrishadrianwong@gmail.com>
2023-09-03 11:22:36 -0700

    Added more horizontal button on the card

--- src/pages/recipe/components/RecipeContent.jsx
+++ src/pages/recipe/components/RecipeContent.jsx
@@ -1,11 +1,52 @@
 import React, { useState } from 'react';
 import './RecipeContent.css';
 import { InstagramEmbed } from 'react-social-media-embed';
-import { Box, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import {
+	Box,
+	Card,
+	CardContent,
+	CardMedia,
+	Grid,
+	IconButton, // Import IconButton
+	Menu, // Import Menu
+	MenuItem, // Import MenuItem
+	Typography,
+} from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'; // Import the triple dots icon
 
 export default function RecipeContent(props) {
 	const { filteredRecipes, palette } = props;
 	const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+	// State for the anchor element of the menu
+	const [anchorEl, setAnchorEl] = useState(null);
+
+	// Function to open the menu
+	const handleMenuOpen = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	// Function to close the menu
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+	};
+
+	// Function to handle the "Update Card" action (you can implement this as needed)
+	const handleUpdateCard = (recipe) => {
+		console.log(recipe);
+		// Add your update card logic here
+		// For example, you can navigate to an update page or show a modal for editing
+		// This function will be called when the "Update Card" option is clicked
+	};
+
+	// Function to handle the "Delete Card" action (you can implement this as needed)
+	const handleDeleteCard = (recipe) => {
+		console.log(recipe);
+		// Add your delete card logic here
+		// For example, you can show a confirmation dialog and delete the card if confirmed
+		// This function will be called when the "Delete Card" option is clicked
+	};
+
 	return (
 		<Box className='flex my-4 h-4/5'>
 			<Box className='w-1/3 flex'>
@@ -49,17 +90,26 @@ export default function RecipeContent(props) {
 									}}
 								/>
 								<CardContent sx={{ height: 0 }}>
-									<Typography
-										variant='h6'
-										component='h2'
-										sx={{
-											color: `${palette.secondary.main}`,
-											fontWeight: 'bold',
-											mb: 1,
-										}}
-									>
-										{recipe.title}
-									</Typography>
+									<Box className='flex justify-between items-center'>
+										<Typography
+											variant='h6'
+											component='h2'
+											sx={{
+												color: `${palette.secondary.main}`,
+												fontWeight: 'bold',
+												mb: 1,
+											}}
+										>
+											{recipe.title}
+										</Typography>
+										<IconButton aria-label='options' onClick={handleMenuOpen}>
+											<MoreHorizIcon />
+										</IconButton>
+										<Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+											<MenuItem onClick={() => handleUpdateCard(recipe)}>Update Card</MenuItem>
+											<MenuItem onClick={() => handleDeleteCard(recipe)}>Delete Card</MenuItem>
+										</Menu>
+									</Box>
 									<Typography variant='body2' component='p' sx={{ color: `${palette.primary.main}` }}>
 										{recipe.description}
 									</Typography>
